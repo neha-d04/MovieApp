084342c64837cebdb3f4856f7974095333f33a16
neha-d04 <68469207+neha-d04@users.noreply.github.com>
2021-12-09 09:55:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let movieList = document.getElementById("movielist");
+
+const apikey = "api_key=de8a2067ee40c32c348f2e57d0ebe418";
+const baseurl = "https://api.themoviedb.org/3";
+const apiurl = `${baseurl}/discover/movie?sort_by=popularity.desc&${apikey}`;
+fetch(`${apiurl}`)
+  .then((response) => response.json())
+  .then((data) => {
+    displayMovie(data.results);
+    console.log(data);
+  });
+
+function displayMovie(data) {
+  let html = "";
+  data.forEach((element) => {
+    html += `<div
+            class="card col-lg-4 col-md-6 col-sm-12 movie"
+            style="width: 22rem"
+          >
+            <img src="https://image.tmdb.org/t/p/w500${element.poster_path}" class="card-img-top" alt="Image" />
+            <div class="card-body">
+              <h5 class="card-title">${element.title}</h5>
+              <p class="card-text">
+                Release Date: ${element.release_date}
+              </p>
+              <span class="badge bg-success">${element.vote_average} </span>
+            </div>
+          </div>`;
+  });
+
+  movieList.innerHTML = html;
+}
